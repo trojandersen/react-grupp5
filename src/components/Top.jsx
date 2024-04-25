050e89cc6f50215cb341680e3f792edb0ca7d2f2
Patrik Eriksson <puttrik96@hotmail.com>
2024-04-25 12:40:32 +0200

    added header styling

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,9 +1,11 @@
+import "../styles/Top.css";
+
 function Top(props) {
   return (
-    <>
+    <header className="header">
       <h1>{props.title}</h1>
       <p>{props.subtitle}</p>
-    </>
+    </header>
   );
 }
 
--- src/pages/AddCard.jsx
+++ src/pages/AddCard.jsx
@@ -6,7 +6,6 @@ import Button from "../components/Button";
 function AddCard({ title, subtitle }) {
   return (
     <>
-      <h1>AddCard</h1>
       <Top title={title} subtitle={subtitle} />
       <Card />
       <CardForm />
