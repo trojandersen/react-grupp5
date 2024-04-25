819431d3636e4dc66c36056337162eff62b82086
Patrik Eriksson <puttrik96@hotmail.com>
2024-04-25 10:11:18 +0200

    added custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bitcoin: "#FFAE34",
+        ninja: "#222222",
+        blockchain: "#8B58F9",
+        evilcorp: "#F33355",
+        placeholdergray: "#D0D0D0",
+      },
+    },
   },
   plugins: [],
-}
-
+};
